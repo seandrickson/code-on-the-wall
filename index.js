1b1d2da35f0ce8a18bfac50e83a2a764b6e12c02
Sean Hendrickson <seandrickson@gmail.com>
2017-04-29 23:00:34 -0400

    Fix functionality and allow passing new code

--- index.js
+++ index.js
@@ -1,47 +1,38 @@
-const querystring = require('querystring');
-const path = require('path');
+const _ = require('lodash');
 const Nightmare = require('nightmare');
+const path = require('path');
+const qs = require('querystringify');
 const wallpaper = require('wallpaper');
 const args = require('yargs').argv;
 
-function removeCommonArgs(args) {
-  const keyBlackList = ['_', '$0'];
-  let newArgs = {};
-
-  for (var i in args) {
-    if (!keyBlackList.includes(i))
-      newArgs[i] = args[i];
-  }
-
-  return newArgs;  
-}
-
-let PAGE_ARGS = removeCommonArgs(args);
-const PAGE_URL = path.resolve(__dirname, './web/index.html');
-const PAGE_PATH = `file:///${PAGE_URL}?${querystring.stringify(PAGE_ARGS)}`;
+const PAGE_ARGS = _.omit(args, ['_', '$0']);
+const PAGE_PATH = path.resolve(__dirname, './web/index.html');
+const PAGE_QUERY = qs.stringify(PAGE_ARGS);
+const PAGE_URL = `file:///${PAGE_PATH}?${PAGE_QUERY}`;
 
 const nightmare = new Nightmare({ show: false });
 
 nightmare
-  .goto(PAGE_PATH)
+  .goto(PAGE_URL)
   .evaluate(() => {
     return {      
       height: window.screen.height,
       width: window.screen.width,
       density: window.devicePixelRatio,
       title: document.title,
+      query: window.location.search,
     };
   })
-  .then(function (page) {
-    const width = page.width * page.density;
-    const height = page.height * page.density;
-    const wallpaperName = `${page.title}_${width}x${height}.png`;
+  .then((page) => {
+    const pixelWidth = page.width * page.density;
+    const pixelHeight = page.height * page.density;
+    const wallpaperName = `${page.title}_${pixelWidth}x${pixelHeight}.png`;
     const wallpaperPath = path.resolve(__dirname, `./output/${wallpaperName}`);
 
-    console.log('Taking snapshot using: ' + querystring.stringify(PAGE_ARGS));
-
+    console.log(`Taking snapshot using: ${page.query}`);
     return nightmare
       .viewport(page.width, page.height)
+      .wait('.dom-ready')
       .wait(1000)
       .screenshot(wallpaperPath, {
         x: 0,
@@ -50,15 +41,16 @@ nightmare
         height: page.height
       })
       .end(() => {
+        console.log(`Wallpaper saved: ${wallpaperPath}`);
         return wallpaperPath;
-      })
+      });
   })
-  .then((res) => {
-    wallpaper.set(res);
-    console.log('Wallpaper now set!');
+  .then((wallpaperPath) => {
+    wallpaper.set(wallpaperPath).then(() => {
+      console.log('Wallpaper now set!');
+      process.exit();
+    });
   }, (err) => {
     console.error(err);
-  })
-  .then(() => {
-    return process.exit();
+    process.exit();
   });
