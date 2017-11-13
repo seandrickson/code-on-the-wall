2b52c07cd8810d4ca279aebbfc36f62909a76f5f
Sean Hendrickson <shendrickson@tree.com>
2017-11-13 15:32:42 -0500

    Now using puppeteer

--- index.js
+++ index.js
@@ -1,84 +1,57 @@
-const _ = require('lodash');
-const Nightmare = require('nightmare');
+const puppeteer = require('puppeteer');
 const path = require('path');
 const qs = require('querystringify');
-const wallpaper = require('wallpaper');
-const args = require('yargs').argv;
+const { _, $0, ...args } = require('yargs').argv;
 const wallConfig = require('./wall-config');
 
-const PAGE_ARGS = _.omit(args, ['_', '$0']);
+const PAGE_ARGS = Object.assign({}, args, wallConfig);
 const PAGE_PATH = path.resolve(__dirname, './dist/index.html');
-let PAGE_QUERY;
-
-if (PAGE_ARGS.length)
-  PAGE_QUERY = qs.stringify(PAGE_ARGS);
-else
-  PAGE_QUERY = qs.stringify(wallConfig);
-
+const PAGE_QUERY = qs.stringify(PAGE_ARGS);
 const PAGE_URL = `file:///${PAGE_PATH}?${PAGE_QUERY}`;
+console.log('Configuration:', PAGE_ARGS);
 
-var nightmareConfig = {
-  show: false
-};
+const viewportObj = ((obj) => {
+  const intKeys = ['deviceScaleFactor', 'height', 'width'];
+  let newObj = {};
+  intKeys.forEach(key => {
+    if (obj[key] !== undefined)
+      newObj[key] = parseInt(String(obj[key]).replace('px', ''));
+  });
+  return newObj;
+})(wallConfig || {});
 
-if (wallConfig && wallConfig.devicePixelRatio) {
-  nightmareConfig.switches = {
-    'force-device-scale-factor': wallConfig.devicePixelRatio
-  }
-}
+(async () => {
+  const browser = await puppeteer.launch({ headless: true });
 
-var pageOverrides = [];
-if (wallConfig && wallConfig.width) {
-  pageOverrides.width = wallConfig.width;
-}
-if (wallConfig && wallConfig.height) {
-  pageOverrides.height = wallConfig.height;
-}
+  const page = await browser.newPage();
+  await page.goto(PAGE_URL);
+  await page.setViewport(viewportObj);
+  await page.waitFor('html.dom-complete');
 
-console.log(`Starting application...`);
-const nightmare = new Nightmare(nightmareConfig);
-nightmare
-  .goto(PAGE_URL)
-  .wait('html.dom-complete')
-  .evaluate(() => {
-    return {      
+  const dimensions = await page.evaluate(() => {
+    return {
       height: window.screen.height,
       width: window.screen.width,
       density: window.devicePixelRatio,
-      title: document.title,
-      query: window.location.search,
+      title: document.title
     };
-  })
-  .then((page) => {
-    var width = parseInt(pageOverrides.width.replace('px','')) || page.width;
-    var height = parseInt(pageOverrides.height.replace('px', '')) || page.height;
-    const pixelWidth = Math.floor(width * page.density);
-    const pixelHeight = Math.floor(height * page.density);
-    const pageTitle = page.title || 'code-on-the-wall';
-    const wallpaperName = `${page.title}_${pixelWidth}x${pixelHeight}.png`;
-    const wallpaperPath = path.resolve(__dirname, `./output/${wallpaperName}`);
-
-    console.log(`Taking snapshot using: Query: ${page.query}, Wallpaper: ${wallpaperName}`);
-    return nightmare
-      .viewport(width, height)
-      .wait(1000)
-      .screenshot(wallpaperPath, {
-        x: 0,
-        y: 0,
-        width: width,
-        height: height
-      })
-      .end(() => {
-        console.log(`Wallpaper saved: ${wallpaperPath}`);
-        // return wallpaperPath;
-      });
-  })
-  .then((wallpaperPath) => {
-    // wallpaper.set(wallpaperPath).then(() => {
-    //   console.log('Wallpaper now set!');
-      process.exit();
-    // });
-  }, (err) => {
-    console.error(err);
-    process.exit();
   });
+  const width = viewportObj.width || dimensions.width;
+  const height = viewportObj.height || dimensions.height;
+  const pixelWidth = Math.floor(width * dimensions.density);
+  const pixelHeight = Math.floor(height * dimensions.density);
+  const wallpaperName = `${dimensions.title}_${pixelWidth}x${pixelHeight}.png`;
+  const wallpaperPath = path.resolve(__dirname, `./output/${wallpaperName}`);
+
+  console.log('Taking screenshot:', {
+    filename: wallpaperName,
+    filepath: wallpaperPath,
+    width: width,
+    height: height,
+    density: dimensions.density
+  });
+  await page.screenshot({ path: wallpaperPath });
+  console.log('Wallpaper saved:', wallpaperPath);
+
+  await browser.close();
+})();
